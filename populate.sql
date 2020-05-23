98c3be082d559bfaf499bab304cbf84d4229a89b
Чебан Ростислав <44871917+ChebanRostislavKM81@users.noreply.github.com>
2020-05-24 01:10:56 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,45 +1,21 @@
+
+
 INSERT INTO TEAM(team)
 VALUES('England');
 INSERT INTO TEAM(team)
 VALUES('Wales');
 INSERT INTO TEAM(team)
 VALUES('Ireland');
-INSERT INTO HOME_TEAM(home_team)
-VALUES('England');
-INSERT INTO HOME_TEAM(home_team)
-VALUES('Wales');
-INSERT INTO HOME_TEAM(home_team)
-VALUES('Ireland');
-INSERT INTO AWAY_TEAM(away_team)
-VALUES('England');
-INSERT INTO AWAY_TEAM(away_team)
-VALUES('Wales');
-INSERT INTO AWAY_TEAM(away_team)
-VALUES('Ireland');
+
 INSERT INTO PLAYER(player_name)
 VALUES('Rooney');
 INSERT INTO PLAYER(player_name)
 VALUES('Bale');
 INSERT INTO PLAYER(player_name)
 VALUES('Duffy');
-INSERT INTO LOCATION(country, city)
-VALUES('England','London');
-INSERT INTO LOCATION(country, city)
-VALUES('Wales','Cardiff');
-INSERT INTO LOCATION(country, city)
-VALUES('Dublin','Ireland');
-INSERT INTO COACH(coach_name)
-VALUES('Southgate');
-INSERT INTO COACH(coach_name)
-VALUES('Davies');
-INSERT INTO COACH(coach_name)
-VALUES('Daniels');
-INSERT INTO TEAM_PLAYER(team, player_name, coach_name)
-VALUES('England', 'Rooney', 'Southgate');
-INSERT INTO TEAM_PLAYER(team, player_name, coach_name)
-VALUES('Wales', 'Bale', 'Davies');
-INSERT INTO TEAM_PLAYER(team, player_name, coach_name)
-VALUES('Ireland', 'Duffy', 'Daniels');
+
+
+
 
 INSERT INTO MATCH(match_id, dates, tournament, neutral)
 VALUES(1, '01.01.70', 'Friendly', 'FALSE');
@@ -47,11 +23,45 @@ INSERT INTO MATCH(match_id, dates, tournament, neutral)
 VALUES(2, '02.01.70', 'Friendly', 'FALSE');
 INSERT INTO MATCH(match_id, dates, tournament, neutral)
 VALUES(3, '03.01.70', 'Friendly', 'FALSE');
+
+INSERT INTO LOCATION(country, city)
+VALUES('England','London');
+INSERT INTO LOCATION(country, city)
+VALUES('Wales','Cardiff');
+INSERT INTO LOCATION(country, city)
+VALUES('Dublin','Ireland');
+
+
+INSERT INTO HOME_TEAM(home_team)
+VALUES('England');
+INSERT INTO HOME_TEAM(home_team)
+VALUES('Wales');
+INSERT INTO HOME_TEAM(home_team)
+VALUES('Ireland');
+
+INSERT INTO COACH(coach_name)
+VALUES('Southgate');
+INSERT INTO COACH(coach_name)
+VALUES('Davies');
+INSERT INTO COACH(coach_name)
+VALUES('Daniels');
+
+INSERT INTO AWAY_TEAM(away_team)
+VALUES('England');
+INSERT INTO AWAY_TEAM(away_team)
+VALUES('Wales');
+INSERT INTO AWAY_TEAM(away_team)
+VALUES('Ireland');
+
 INSERT INTO PLAY(match_id, city, country, home_team, away_team)
 VALUES(1, 'London', 'England', 'England', 'Wales');
 INSERT INTO PLAY(match_id, city, country, home_team, away_team)
 VALUES(2, 'Cardiff', 'Wales', 'Wales', 'Ireland');
-INSERT INTO PLAY(match_id, city, country, home_team, away_team)
-VALUES(3, 'Dublin', 'Ireland', 'Ireland', 'England');
 
 
+INSERT INTO TEAM_PLAYER(team, player_name, coach_name)
+VALUES('England', 'Rooney', 'Southgate');
+INSERT INTO TEAM_PLAYER(team, player_name, coach_name)
+VALUES('Wales', 'Bale', 'Davies');
+INSERT INTO TEAM_PLAYER(team, player_name, coach_name)
+VALUES('Ireland', 'Duffy', 'Daniels');
